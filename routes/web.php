9624ca577d43655d6485157bbc15de6eb163a756
Michael Watts <michael.watts25@outlook.com>
2020-06-29 12:06:41 +0100

    The project now uses a submission for allows the user to add their favourite links which goes into a database. i have also added test cases for the migration data.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Link::all();
+
+    return view('welcome', ['links' => $links]);
+});
+
+Route::get('/submitForm', function () {
+    return view('submit');
+});
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
 });
 
 Auth::routes();
